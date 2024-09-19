0a9fd0b3ce770398f3ea336c5b794ccf3b6e0571
mareks-miksta <mareks.miksta@valmiera.edu.lv>
2024-09-19 15:04:15 +0300

    work python code main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Sveika pasaule")
+class Automašīna:
+
+    def __init__(self, marka, modelis, krasa):
+
+        self.marka = marka
+        self.modelis = modelis
+        self.krasa = krasa
+
+    def tas(self):
+        print("Tas ir", self.marka, self.modelis, self.krasa)
+
+Audi = Automašīna("Audi", "A4", "Sarkanā krāsa")
+BMW = Automašīna("BMW", "X5", "Melnā krāsa")
+
+Audi.tas()
+
+BMW.tas()
